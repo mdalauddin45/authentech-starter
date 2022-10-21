f1e35868d16e105b7ec9c4b17521efea53bc0dce
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-21 10:02:17 +0600

    dfghdg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div className='flex justify-center items-center min-h-screen'>
-      <h1 className='text-3xl'>Welcome to Authentech</h1>
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-3xl">Welcome to Authentech</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
